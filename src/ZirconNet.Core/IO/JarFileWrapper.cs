eec756052b16227a12821b3f3dd3a781ccbda075
Nicolas Simoneau <simnico99@hotmail.com>
2022-07-07 22:41:23 -0400

    Added JarFileWrapper

---
+++ src/ZirconNet.Core/IO/JarFileWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZirconNet.Core.IO;
+public class JarFileWrapper : FileWrapper
+{
+    public JarFileWrapper(string file, bool createFile = true, bool overwrite = false) : base(file, createFile, overwrite) { }
+    public JarFileWrapper(FileInfo file, bool createFile = true, bool overwrite = false) : base(file, createFile, overwrite) { }
+
+    public Process Run()
+    {
+        Process clientProcess = new();
+
+        clientProcess.StartInfo = new ProcessStartInfo
+        {
+            FileName = "java",
+            Arguments = $@"-jar {FullName} ",
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+        clientProcess.Start();
+
+        return clientProcess;
+    }
+}
