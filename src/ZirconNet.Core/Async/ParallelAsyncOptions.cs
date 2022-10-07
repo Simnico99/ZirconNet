498bae4f43cfb08495fc2eb393df6a3169934829
Simnico99 <Simnico99@hotmail.com>
2022-10-07 14:38:38 -0400

    Added default ParrallelAsyncOptions

--- src/ZirconNet.Core/Async/ParallelAsyncOptions.cs
+++ src/ZirconNet.Core/Async/ParallelAsyncOptions.cs
@@ -1,6 +1,8 @@
 ﻿namespace ZirconNet.Core.Async;
 public readonly struct ParallelAsyncOptions
 {
+    private static ParallelAsyncOptions? _internalDefaultStuct;
+    public static ParallelAsyncOptions Default => _internalDefaultStuct ??= new();
 #if NET5_0_OR_GREATER
     public CancellationToken CancellationToken { get; init; } = default;
     public int MaxDegreeOfParallelism { get; init; } = -1;
