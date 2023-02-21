72f51bccdb813649bd7e6e26c8115e3cf01d8860
Simnico99 <Simnico99@hotmail.com>
2023-02-21 08:56:33 -0500

    Added empty readonly collections/dictionnary helpers.

---
+++ src/ZirconNet.Core/Collections/EmptyCollectionHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZirconNet.Core.Collections;
+
+//Not needed in Net8.0 as they added ReadOnlyCollections
+#if NETFRAMEWORK || NET5_0 || NET6_0 || NET7_0
+public static class EmptyCollectionHelper<T>
+{
+    public static ReadOnlyCollection<T> ReadOnlyCollection { get; } = new ReadOnlyCollection<T>(Array.Empty<T>());
+
+    public static ReadOnlyObservableCollection<T> ReadOnlyObservableCollection { get; } = new ReadOnlyObservableCollection<T>(new ObservableCollection<T>());
+}
+
+public static class EmptyDictionnaryHelper<TKey, TValue>
+{
+    public static ReadOnlyDictionary<TKey, TValue> ReadOnlyDictionary { get; } = new ReadOnlyDictionary<TKey, TValue>(new Dictionary<TKey, TValue>());
+}
+#endif
