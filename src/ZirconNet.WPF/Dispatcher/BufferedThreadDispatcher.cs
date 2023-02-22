852f11d2ea28716aa26a4b131af2fdd292657d84
Simnico99 <Simnico99@hotmail.com>
2023-02-22 09:43:59 -0500

    BufferedThreadDispatcher Optimization test.

--- src/ZirconNet.WPF/Dispatcher/BufferedThreadDispatcher.cs
+++ src/ZirconNet.WPF/Dispatcher/BufferedThreadDispatcher.cs
@@ -1,90 +1,129 @@
-﻿using System.Collections.Concurrent;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Threading;
 
 namespace ZirconNet.WPF.Dispatcher;
 
 /// <summary>
-/// Prevent the ui freeze by using a buffer to execute the UI Updates from multiple threads in a sort of queue.,
+/// Prevent the ui freeze by using a buffer to execute the UI Updates from multiple threads in a sort of queue.
 /// </summary>
 public sealed class BufferedThreadDispatcher
 {
-    public static BufferedThreadDispatcher Current { get; } = new();
+    private static readonly BufferedThreadDispatcher _current = new();
 
-    /// <summary>
-    /// Delay to wait between the screen refresh.
-    /// </summary>
+    public static BufferedThreadDispatcher Current => _current;
     public TimeSpan Delay { get; set; } = TimeSpan.FromMilliseconds(1);
 
     private readonly int _mainThreadId;
     private readonly System.Windows.Threading.Dispatcher _dispatcher;
-    private readonly ConcurrentQueue<Action> _queue = new();
+    private readonly Queue<Action> _queue = new(25);
+    private readonly object _queueLock = new();
+    private readonly Thread _uiThread;
+    private readonly TaskCompletionSource<object?> _taskCompletionSource = new();
 
     private BufferedThreadDispatcher()
     {
         _mainThreadId = Application.Current.Dispatcher.Thread.ManagedThreadId;
         _dispatcher = Application.Current.Dispatcher;
-        Task.Run(ProcessQueue);
+
+        _uiThread = new Thread(ProcessQueue);
+        _uiThread.SetApartmentState(ApartmentState.STA);
+        _uiThread.IsBackground = true;
+        _uiThread.Start();
     }
 
     private async void ProcessQueue()
     {
-        while (true)
+        var timer = new Timer(_ => ExecuteQueuedActions(), null, 0, (int)Delay.TotalMilliseconds);
+
+        await _taskCompletionSource.Task;
+    }
+
+    private void ExecuteQueuedActions()
+    {
+        Action? action = null;
+
+        lock (_queueLock)
         {
-            if (_queue.TryDequeue(out var action))
+            if (_queue.Count > 0)
             {
-                if (_mainThreadId == Environment.CurrentManagedThreadId)
+                action = _queue.Dequeue();
+            }
+        }
+
+        while (action != null)
+        {
+            if (_mainThreadId == Environment.CurrentManagedThreadId)
+            {
+                action();
+            }
+            else
+            {
+                _dispatcher.Invoke(action, DispatcherPriority.Send);
+            }
+
+            action = null;
+
+            lock (_queueLock)
+            {
+                if (_queue.Count > 0)
                 {
-                    action();
-                }
-                else
-                {
-                    _dispatcher.Invoke(action, DispatcherPriority.Send);
+                    action = _queue.Dequeue();
                 }
             }
-            await Task.Delay(Delay);
         }
     }
 
     public void Invoke(Action action)
     {
-        _queue.Enqueue(action);
+        lock (_queueLock)
+        {
+            _queue.Enqueue(action);
+        }
     }
 
     public async Task<T> InvokeAsync<T>(Func<T> func)
     {
         var tcs = new TaskCompletionSource<T>();
-        _queue.Enqueue(() =>
+
+        lock (_queueLock)
         {
-            try
+            _queue.Enqueue(() =>
             {
-                var result = func();
-                tcs.SetResult(result);
-            }
-            catch (Exception ex)
-            {
-                tcs.SetException(ex);
-            }
-        });
-        return await tcs.Task;
+                try
+                {
+                    var result = func();
+                    tcs.SetResult(result);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+            });
+        }
+
+        return await tcs.Task.ConfigureAwait(false);
     }
 
     public async Task InvokeAsync(Action act)
     {
-        var tcs = new TaskCompletionSource<object?>();
-        _queue.Enqueue(() =>
+        var tcs = new TaskCompletionSource<object>();
+
+        lock (_queueLock)
         {
-            try
+            _queue.Enqueue(() =>
             {
-                act();
-                tcs.SetResult(null);
-            }
-            catch (Exception ex)
-            {
-                tcs.SetException(ex);
-            }
-        });
-        await tcs.Task;
+                try
+                {
+                    act();
+                    tcs.SetResult(default!);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+            });
+        }
+
+        await tcs.Task.ConfigureAwait(false);
     }
 }
