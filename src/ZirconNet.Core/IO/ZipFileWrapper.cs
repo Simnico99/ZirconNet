43852d6a8990d63208ba8bc23e0f28791dc906ee
Simnico99 <simnico99@hotmail.com>
2023-08-11 14:03:03 -0400

    Optimized Zip files to support parallel extraction

--- src/ZirconNet.Core/IO/ZipFileWrapper.cs
+++ src/ZirconNet.Core/IO/ZipFileWrapper.cs
@@ -2,10 +2,9 @@
 // This software is distributed under the MIT license and its code is open-source and free for use, modification, and distribution.
 // </copyright>
 
+using System.Buffers;
 using System.IO.Compression;
-#if NET5_0_OR_GREATER
-using System.Runtime.Versioning;
-#endif
+using ZirconNet.Core.Async;
 using ZirconNet.Core.Events;
 
 namespace ZirconNet.Core.IO;
@@ -13,6 +12,8 @@ namespace ZirconNet.Core.IO;
 public sealed class ZipFileWrapper : FileWrapperBase
 {
     private const char _slash = '/';
+
+    private int? _cachedCount;
 
     public ZipFileWrapper(string file, bool createFile = true, bool overwrite = false)
     : base(file, createFile, overwrite)
@@ -31,32 +32,75 @@ public sealed class ZipFileWrapper : FileWrapperBase
     public async ValueTask UnzipAsync(string extractionPath)
     {
         using var archive = ZipFile.OpenRead(FullName);
-        foreach (var zipArchiveEntry in archive.Entries)
+
+        await archive.Entries.ForEach(Environment.ProcessorCount, async (zipArchiveEntry) =>
         {
-            var normalizedPath = zipArchiveEntry.FullName.Replace(_slash, Path.DirectorySeparatorChar);
-            var extractionName = normalizedPath[FullName.Length..];
-            var extractionPathFullName = Path.Combine(extractionPath, extractionName);
-
-            Extracting.Publish(extractionName);
-
-            if (!normalizedPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.InvariantCulture) && !string.IsNullOrWhiteSpace(zipArchiveEntry.Name))
-            {
-                using var stream = zipArchiveEntry.Open();
-                using var fileStream = File.Create(extractionPathFullName);
-                await stream.CopyToAsync(fileStream).ConfigureAwait(false);
-            }
-            else
-            {
-                _ = Directory.CreateDirectory(extractionPathFullName);
-            }
-
-            Extracted.Publish(extractionName);
-        }
+            var (normalizedPath, extractionName, extractionPathFullName) = PreparePaths(zipArchiveEntry.FullName, extractionPath);
+            await ExtractEntryAsync(normalizedPath, extractionPathFullName, extractionName, zipArchiveEntry).ConfigureAwait(false);
+        }).ConfigureAwait(false);
     }
 
     public int Count()
     {
-        using var archive = ZipFile.OpenRead(FullName);
-        return archive.Entries.Count;
+        if (!_cachedCount.HasValue)
+        {
+            using var archive = ZipFile.OpenRead(FullName);
+            _cachedCount = archive.Entries.Count;
+        }
+
+        return _cachedCount.Value;
+    }
+
+    private async Task ExtractEntryAsync(string normalizedPath, string extractionPathFullName, string extractionName, ZipArchiveEntry zipArchiveEntry)
+    {
+        Extracting.Publish(extractionName);
+
+        if (normalizedPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+        {
+            CreateDirectoryIfNeeded(extractionPathFullName);
+        }
+        else
+        {
+            await ExtractFileAsync(extractionPathFullName, zipArchiveEntry).ConfigureAwait(false);
+        }
+    }
+
+    private void CreateDirectoryIfNeeded(string extractionPathFullName)
+    {
+        if (!Directory.Exists(extractionPathFullName))
+        {
+            _ = Directory.CreateDirectory(extractionPathFullName);
+        }
+    }
+
+    private async Task ExtractFileAsync(string extractionPathFullName, ZipArchiveEntry zipArchiveEntry)
+    {
+        if (File.Exists(extractionPathFullName))
+        {
+            using var stream = zipArchiveEntry.Open();
+            using var fileStream = new FileStream(extractionPathFullName, FileMode.Create, FileAccess.Write, FileShare.None, bufferSize: 4096, useAsync: true);
+            var buffer = ArrayPool<byte>.Shared.Rent(4096);
+            try
+            {
+                int bytesRead;
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false)) != 0)
+                {
+                    await fileStream.WriteAsync(buffer, 0, bytesRead).ConfigureAwait(false);
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+    }
+
+    private (string NormalizedPath, string ExtractionName, string ExtractionPathFullName) PreparePaths(string originalPath, string extractionPath)
+    {
+        var normalizedPath = originalPath.Replace(_slash, Path.DirectorySeparatorChar);
+        var extractionName = normalizedPath[FullName.Length..];
+        var extractionPathFullName = Path.Combine(extractionPath, extractionName);
+
+        return (normalizedPath, extractionName, extractionPathFullName);
     }
 }
