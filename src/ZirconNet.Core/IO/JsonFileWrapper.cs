7014af7331187a3e8005171166b305b9fb1be3e7
Simnico99 <simnico99@hotmail.com>
2024-04-02 21:05:16 -0400

    Updated JsonFileWrapper

--- src/ZirconNet.Core/IO/JsonFileWrapper.cs
+++ src/ZirconNet.Core/IO/JsonFileWrapper.cs
@@ -4,222 +4,100 @@
 
 using System.Text.Json;
 using System.Text.Json.Nodes;
-using ZirconNet.Core.Async;
 
 namespace ZirconNet.Core.IO;
 
 /// <summary>
 /// Should be disposed after use.
 /// </summary>
-public sealed class JsonFileWrapper : FileWrapperBase<JsonFileWrapper>, IDisposable
+public sealed class JsonFileWrapper : FileWrapperBase<JsonFileWrapper>
 {
-    private readonly StreamReader _reader;
-    private readonly LockAsync _asyncLock = new();
-
-    private JsonObject _fileContent = [];
-    private bool _disposedValue;
-    private bool _hasFlushedLastChanges;
+    private readonly JsonObject _fileContent;
 
     public JsonFileWrapper(string file, bool createFile = true, bool overwrite = false)
         : base(file, createFile, overwrite)
     {
-        _reader = new StreamReader(FullName);
+        var reader = File.OpenRead(FullName);
+        if (reader.Length == 0)
+        {
+            var newDocument = JsonDocument.Parse("{}");
+            _fileContent = newDocument.Deserialize<JsonObject>()!;
+            return;
+        }
+
+        var document = JsonDocument.Parse(reader);
+        _fileContent = document.Deserialize<JsonObject>()!;
     }
 
     public JsonFileWrapper(FileInfo file, bool createFile = true, bool overwrite = false)
         : base(file, createFile, overwrite)
     {
-        _reader = new StreamReader(FullName);
-    }
-
-    public void DeleteKey(string fieldToDelete)
-    {
-        _hasFlushedLastChanges = false;
-        if (_fileContent is null)
+        var reader = File.OpenRead(FullName);
+        if (reader.Length == 0)
         {
+            var newDocument = JsonDocument.Parse("{}");
+            _fileContent = newDocument.Deserialize<JsonObject>()!;
             return;
         }
 
-        lock (_fileContent)
-        {
-            if (_fileContent?[fieldToDelete] is not null)
-            {
-                _fileContent = _fileContent is not null ? _fileContent : JsonNode.Parse("{  }")?.AsObject()!;
+        var document = JsonDocument.Parse(reader);
+        _fileContent = document.Deserialize<JsonObject>()!;
+    }
 
-                _ = _fileContent?.Remove(fieldToDelete);
-            }
+    public T? ReadKey<T>(string key)
+    {
+        return _fileContent[key].Deserialize<T>();
+    }
+
+    public bool TryReadKey<T>(string key, out T? result)
+    {
+        result = default;
+        if (_fileContent.TryGetPropertyValue(key, out var element))
+        {
+            result = element.Deserialize<T>();
+            return true;
+        }
+
+        return false;
+    }
+
+    public void ModifyKey<T>(string key, T newValue)
+    {
+        if (_fileContent.ContainsKey(key))
+        {
+            _fileContent[key] = JsonNode.Parse(JsonSerializer.Serialize(newValue));
+        }
+        else
+        {
+            throw new KeyNotFoundException($"The key '{key}' was not found in the JSON file.");
         }
     }
 
-    public void ModifyKey(string fieldToModify, object value)
+    public void AddKey<T>(string key, T value)
     {
-        _hasFlushedLastChanges = false;
-        if (_fileContent is null)
-        {
-            return;
-        }
-
-        lock (_fileContent)
-        {
-            if (_fileContent?[fieldToModify] is not null)
-            {
-                _ = _fileContent.AsObject().Remove(fieldToModify);
-            }
-            else
-            {
-                AddKey(fieldToModify, value);
-            }
-        }
+        _fileContent[key] = JsonNode.Parse(JsonSerializer.Serialize(value));
     }
 
-    public JsonObject GetJsonObject()
+    public void DeleteKey(string key)
     {
-        return _fileContent is null ? [] : _fileContent;
+        _fileContent.Remove(key);
     }
 
-    public void MergeJsonObject(JsonObject jsonObject)
+    public void Flush()
     {
-        _hasFlushedLastChanges = false;
-        _ = _fileContent?.Concat(jsonObject);
+        using var stream = new FileStream(FullName, FileMode.Create, FileAccess.Write, FileShare.None);
+        using var writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true });
+        _fileContent.WriteTo(writer);
+        writer.Flush();
     }
 
-    public void Clear()
+#if NET6_0_OR_GREATER
+    public Task FlushAsync()
     {
-        _hasFlushedLastChanges = false;
-        _fileContent = [];
-    }
-
-    public async Task LoadFileAsync(bool forceRead = false)
-    {
-        await _asyncLock.Lock<Task>(async () => await LoadFileInternalAsync(forceRead)).ConfigureAwait(false);
-    }
-
-    public void LoadFile(bool forceRead = false)
-    {
-        LoadFileInternalAsync(forceRead).GetAwaiter().GetResult();
-    }
-
-    public async ValueTask<(T? Result, bool Success)> ReadKeyAsync<T>(string fieldName, bool forceRead = false)
-    {
-        await LoadFileAsync(forceRead).ConfigureAwait(false);
-
-        if (_fileContent?[fieldName] is not null)
-        {
-            try
-            {
-                var field = _fileContent?[fieldName];
-                if (field is not null)
-                {
-                    var success = field.AsValue().TryGetValue<T>(out var content);
-                    return (content, success);
-                }
-            }
-            catch (FormatException)
-            {
-                return (default, false);
-            }
-        }
-
-        return (default, false);
-    }
-
-    public void WriteAndFlush()
-    {
-        if (_fileContent is not null)
-        {
-            _hasFlushedLastChanges = true;
-            File.WriteAllText(FullName, _fileContent.ToString());
-        }
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-#if NET5_0_OR_GREATER
-    public async Task WriteAndFlushAsync()
-    {
-        if (_fileContent is not null)
-        {
-            _hasFlushedLastChanges = true;
-            await File.WriteAllTextAsync(FullName, _fileContent.ToString()).ConfigureAwait(false);
-        }
+        using var stream = new FileStream(FullName, FileMode.Create, FileAccess.Write, FileShare.None);
+        using var writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true });
+        _fileContent.WriteTo(writer);
+        return writer.FlushAsync();
     }
 #endif
-
-    private async Task LoadFileInternalAsync(bool forceRead)
-    {
-        if ((_fileContent == null || forceRead) && !await IsFileLockedAsync().ConfigureAwait(false))
-        {
-            try
-            {
-                _fileContent = JsonNode.Parse(await _reader.ReadToEndAsync().ConfigureAwait(false))?.AsObject() ?? [];
-            }
-            catch (IOException)
-            {
-                _fileContent = [];
-            }
-            catch (JsonException)
-            {
-                _fileContent = [];
-            }
-        }
-    }
-
-    private void AddKey(string fieldToAdd, object value)
-    {
-        if (_fileContent is null)
-        {
-            return;
-        }
-
-        lock (_fileContent)
-        {
-            if (_fileContent?[fieldToAdd] is null)
-            {
-                _fileContent = _fileContent is not null ? _fileContent : JsonNode.Parse("{  }")?.AsObject()!;
-
-                _fileContent?.Add(fieldToAdd, value.ToString());
-            }
-        }
-    }
-
-    private async Task<bool> IsFileLockedAsync()
-    {
-        for (var attempt = 0; attempt < 10; attempt++)
-        {
-            try
-            {
-                using var stream = File.Open(FileInfo.FullName, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
-                return false;
-            }
-            catch (IOException)
-            {
-                await Task.Delay(1000).ConfigureAwait(false);
-            }
-        }
-
-        throw new IOException($"The current process cannot access the file: '{FullName}' because it is being used by another process!");
-    }
-
-    private void Dispose(bool disposing)
-    {
-        if (!_disposedValue)
-        {
-            if (disposing)
-            {
-                if (!_hasFlushedLastChanges)
-                {
-                    WriteAndFlush();
-                }
-
-                _asyncLock?.Dispose();
-                _reader?.Dispose();
-            }
-
-            _disposedValue = true;
-        }
-    }
 }
