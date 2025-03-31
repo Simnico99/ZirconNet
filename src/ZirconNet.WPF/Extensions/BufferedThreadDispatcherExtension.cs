b82940e9e062cf3ba87711591a3f1fed357d9c17
Simnico99 <simnico99@hotmail.com>
2025-03-31 03:29:52 -0400

    Added HostApplicationBuilder to the bufferedThreadDispatcher.

--- src/ZirconNet.WPF/Extensions/BufferedThreadDispatcherExtension.cs
+++ src/ZirconNet.WPF/Extensions/BufferedThreadDispatcherExtension.cs
@@ -11,6 +11,18 @@ namespace ZirconNet.WPF.Extensions;
 
 public static class BufferedThreadDispatcherExtension
 {
+#if NET6_0_OR_GREATER
+    public static HostApplicationBuilder UseBufferedThreadDispatcher(this HostApplicationBuilder builder, TimeSpan delay = default!)
+    {
+        if (delay != default!)
+        {
+            BufferedThreadDispatcher.Current.Delay = delay;
+        }
+
+        builder.Services.AddBackgroundServices<BufferedDispatcherBackgroundService>();
+        return builder;
+    }
+#else
     public static IHostBuilder UseBufferedThreadDispatcher(this IHostBuilder builder, TimeSpan delay = default!)
     {
         if (delay != default!)
@@ -21,4 +33,6 @@ public static class BufferedThreadDispatcherExtension
         builder.ConfigureServices(services => services.AddBackgroundServices<BufferedDispatcherBackgroundService>());
         return builder;
     }
+
+#endif
 }
